b49702a12c8658a063cd2a407ca25648563b5eab
helinguler <gulerhelin@hotmail.com>
2024-08-06 18:04:51 +0300

    I am rich app completed

---
+++ angela-yu-lessons/Ch02-IamRichApp/IamRichApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IamRichApp
+//
+//  Created by Helin Güler on 6.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
